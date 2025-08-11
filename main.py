01d4a1c58c9163c073bae1bc21e0b8989afa7c49
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-11 23:31:27 +0530

    importing necessary packages

---
+++ main.py
@@ -0,0 +1,5 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+import pandas as pd
